f47b3b13efd37fac0c158db1d89c2994eee29a00
Mayur Upadhayay <mayur123a@gmail.com>
2022-11-24 17:06:43 +0530

    chore: Refactoring the code

--- frame-script.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	window.onload = function() {
-		let payload = {
-			height: document.body.scrollHeight
-		};
-
-		window.top.postMessage(payload, '*');
-	}
-})();
--- index.js
+++ index.js
@@ -3,43 +3,48 @@
 
 	// let the root document load
 	window.onload = function() {
-		// Generate and render an iframe element
-		renderIframe();
+		// 1. Generate and render an iframe element
+		const iframeContainer = document.querySelector('[data-frame-id]');
 
+		renderIframe();
+		// generates an iframe element and attaches it to the DOM
+		function renderIframe() {
+			// create an iframe
+			const iframeEl = document.createElement('iframe');
+
+			// set minimum iframe dimensions
+			iframeEl.height = "100%";
+			iframeEl.width = "100%";
+
+			iframeEl.name = iframeName;
+			iframeEl.src = iframeContainer.dataset.frameSrc;
+			iframeEl.loading = "lazy";
+			iframeEl.allowfullscren = "allowfullscren";
+			iframeEl.style.margin = 0;
+			iframeEl.scrolling = "no";
+
+			iframeContainer.appendChild(iframeEl);
+		}
+
+		// create a reference to iframe element
 		const iframeEl = document.querySelector(`[name=${iframeName}]`);
+
+		// wait for iframe to load before attaching message event listener
 		iframeEl.onload = function() {
 			// listen for the mesages sent by iframe contents
 			window.addEventListener('message', handleIframeMsgEvent);
 		}
 
-	}
 
-	// generates an iframe element and attaches it to the DOM
-	function renderIframe() {
-		// create an iframe
-		const iframeContainer = document.querySelector('[data-frame-id=iframe]');
-		const iframeEl = document.createElement('iframe');
-
-		// set iframe dimensions
-		iframeEl.height = "300";
-		iframeEl.width = "100%";
-
-		iframeEl.name = iframeName;
-		iframeEl.src = iframeContainer.dataset.frameSrc;
-		iframeEl.loading = "lazy";
-		iframeEl.allowfullscren = "allowfullscren";
-		iframeEl.style.margin = 0;
-		iframeEl.scrolling = "no";
-
-		iframeContainer.appendChild(iframeEl);
-	}
-
-	// waits for a mesage event on iframe
-	function handleIframeMsgEvent(msgEvent) {
-		const iframeEl = document.querySelector(`[name=${iframeName}]`);
-		console.log(msgEvent);
-		if ('height' in msgEvent.data) {
-			iframeEl.height = msgEvent.data.height;
+		// waits for a mesage event on iframe
+		function handleIframeMsgEvent(msgEvent) {			
+			if (msgEvent.data?.hasOwnProperty('height')) {
+				const newHeight = msgEvent.data.height;
+				
+				iframeEl.height = newHeight;
+			}
 		}
+
 	}
+	
 })();
